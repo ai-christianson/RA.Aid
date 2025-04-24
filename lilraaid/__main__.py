c3357c517bf14a3425cdc03947a19c46cbc93b0f
AI Christianson <ai.christianson@christianson.ai>
2025-04-24 15:40:35 -0400

    lilraaid agent

--- lilraaid/__main__.py
+++ lilraaid/__main__.py
@@ -1,8 +1,14 @@
 import sys
 
+from smolagents import CodeAgent, LiteLLMModel, DuckDuckGoSearchTool, tool
+
 def main():
-    print("hello world")
-    sys.exit(0)
+    model = LiteLLMModel(
+        model_id="openrouter/thudm/glm-4-32b:free",
+        temperature=0.2
+    )
+    agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
+    agent.run("How many seconds would it take for a leopard at full speed to run through Pont des Arts?")
 
 if __name__ == "__main__":
     main()
